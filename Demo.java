029fc65e95d7484bcf6ba4a39553cc9381c8bf78
Priyankashinde7 <48512951+Priyankashinde7@users.noreply.github.com>
2023-04-25 14:59:35 +0530

    Array programs

---
+++ Demo.java
@@ -0,0 +1,17 @@
+class  Demo
+{
+	public static void main(String[] args) 
+	{
+		int i =32000;
+		System.out.println("Hello World!");
+
+		nm1(i);
+	}
+
+	static void nm1(int t){
+
+		int x=t;
+
+		System.out.println("Hello World!"+x);
+	}
+}
